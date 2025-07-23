0b2b8ba31b0d57ecb3fd0b4e09e2b6fdc2c99ff2
Esther Eze <estherakowe@yahoo.com>
2025-07-23 05:32:03 +0100

    update app to Laravel 9

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,46 @@
+<!-- Navigation Start -->
+<nav class="navbar bg-white shadow-1 position-fixed top-0 start-0 end-0" style="z-index: 1050;">
+
+      <div class="container d-flex justify-content-between align-items-center py-3">
+            <div class="d-flex align-items-center gap-2">
+                  <a href="{{url('/')}}" class="navbar-brand  ms-lg-0 text-primary">
+                        <img src="/front/img/logo.png" alt="" width="75"> <strong class="text-primary">Local
+                              Eats
+                              Africa</strong>
+                  </a>
+            </div>
+
+            <div>
+                  <a class="btn btn-success fw-bold" href="https://localeats.africa/join-our-rider" role="button">Start Earning Today</a>
+            </div>
+      </div>
+
+</nav>
+
+<!-- Hero Section Start -->
+<section class="py-5 mt-5" id="hero">
+      <div class="container">
+            <div class="row align-items-center">
+
+                  <!-- Left: Text  -->
+                  <div
+                        class="col-lg-6 mb-5 mb-lg-0 d-flex flex-column align-items-center align-items-lg-start text-center text-lg-start">
+                        <h1 class="fw-bold text-dark">
+                              Deliver Food. Earn Fast. On Your Schedule.
+                        </h1>
+                        <p class="text-dark fs-5 mb-4">
+                              Join LocalEats Africa and start earning income your way — with no boss, no office, and the
+                              freedom to work when it works for you.
+                        </p>
+                  </div>
+
+                  <!-- Right: Image -->
+                  <div class="col-lg-6 text-center text-lg-end d-none d-lg-block">
+                        <img src="/front/img/delivery-man.png" class="img-fluid rounded" alt="Delivery man"
+                              style="max-width: 100%; height: auto;">
+                  </div>
+
+
+            </div>
+      </div>
+</section>
